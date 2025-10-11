97432617d6b9eb54c63ddca189c4950a2ee647fc
Abdel-Abbaoui <abdelmjid.abbaoui@etu.uca.fr>
2025-10-11 02:48:53 -0800

    first version :skicare-backend

---
+++ src/main/java/com/hackthon/skicare/SkicareApplication.java
@@ -0,0 +1,13 @@
+package com.hackthon.skicare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkicareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkicareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackthon/skicare/SkicareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackthon.skicare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkicareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
